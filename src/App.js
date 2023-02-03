ced8132f48bb01345fa333fd19bf449207727c1f
Nycole Ribeiro <rpnyck@gmail.com>
2023-02-03 17:23:33 -0300

    feat: Create Login Interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+
+            <span className="login-form-title">Currículo Web</span>
+
+            <div className="wrap-input">
+              <input 
+              className={email !== "" ? "input has-val" : "input"}
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)} />
+              <span className="focus-input" data-placeholder="Email" />
+            </div>
+
+            <div className="wrap-input">
+              <input 
+                className={password !== "" ? "input has-val" : "input"}
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}/>
+              <span className="focus-input" data-placeholder="Senha" />
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Entrar</button>
+            </div>
+
+            <div className="text-center">
+              <span className="text1">Não possui conta?</span>
+              <a className="text2" href="#">Criar conta.</a>
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
